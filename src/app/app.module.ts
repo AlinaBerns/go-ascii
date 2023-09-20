6a799e373b94491619d96137f78ec15cca103743
AlinaBerns <123542234+AlinaBerns@users.noreply.github.com>
2023-09-20 10:15:34 +0200

    +

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { AsciiConverterComponent } from './ascii-converter/ascii-converter.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    AsciiConverterComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/ascii-converter/ascii-converter.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ascii-converter',
+  templateUrl: './ascii-converter.component.html',
+  styleUrls: ['./ascii-converter.component.css']
+})
+export class AsciiConverterComponent {
+
+}
